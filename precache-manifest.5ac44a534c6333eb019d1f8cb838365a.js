5787585987a1d945c040dd24ce766973f67098c0
Calvin Chu <calvinhoyinchu@gmail.com>
2020-08-23 06:38:14 +0800

    Updates

---
+++ precache-manifest.5ac44a534c6333eb019d1f8cb838365a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40bf8947bccde1e348158f458aea597",
+    "url": "/YourTwitchClips/index.html"
+  },
+  {
+    "revision": "020e972d4ba1af175fcc",
+    "url": "/YourTwitchClips/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8107548b2b712e437b19",
+    "url": "/YourTwitchClips/static/css/main.3030c341.chunk.css"
+  },
+  {
+    "revision": "020e972d4ba1af175fcc",
+    "url": "/YourTwitchClips/static/js/2.70c588d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/YourTwitchClips/static/js/2.70c588d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8107548b2b712e437b19",
+    "url": "/YourTwitchClips/static/js/main.b71c86dd.chunk.js"
+  },
+  {
+    "revision": "8b681ecce4f58b699803",
+    "url": "/YourTwitchClips/static/js/runtime-main.6605ad2b.js"
+  },
+  {
+    "revision": "08d98858019e589881b6ec93d16567df",
+    "url": "/YourTwitchClips/static/media/icon.08d98858.png"
+  }
+]);
