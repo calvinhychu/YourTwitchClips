d80d06b7c0dae86c8fbf19912345a99b040b9a0d
Calvin Chu <calvinhoyinchu@gmail.com>
2020-08-23 07:45:29 +0800

    Updates

--- precache-manifest.bbfa0bcd2b633e92b41192e708f1ef50.js
+++ precache-manifest.bbfa0bcd2b633e92b41192e708f1ef50.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c2b2d4efc08817a0f03e3f0042ee5d0",
+    "revision": "0393cbe54613758dd5eb7e07bd9778e7",
     "url": "/YourTwitchClips/index.html"
   },
   {
-    "revision": "020e972d4ba1af175fcc",
+    "revision": "46eabdc395c96c0928a3",
     "url": "/YourTwitchClips/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "d21de4a0985c13df583c",
+    "revision": "86d4babae068574c52b6",
     "url": "/YourTwitchClips/static/css/main.3030c341.chunk.css"
   },
   {
-    "revision": "020e972d4ba1af175fcc",
-    "url": "/YourTwitchClips/static/js/2.70c588d2.chunk.js"
+    "revision": "46eabdc395c96c0928a3",
+    "url": "/YourTwitchClips/static/js/2.47656167.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/YourTwitchClips/static/js/2.70c588d2.chunk.js.LICENSE.txt"
+    "url": "/YourTwitchClips/static/js/2.47656167.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d21de4a0985c13df583c",
-    "url": "/YourTwitchClips/static/js/main.63234655.chunk.js"
+    "revision": "86d4babae068574c52b6",
+    "url": "/YourTwitchClips/static/js/main.2a5da338.chunk.js"
   },
   {
     "revision": "8b681ecce4f58b699803",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/YourTwitchClips/precache-manifest.c840fb3fa289173861420b7d20d7d571.js"
+  "/YourTwitchClips/precache-manifest.bbfa0bcd2b633e92b41192e708f1ef50.js"
 );
 
 self.addEventListener('message', (event) => {
